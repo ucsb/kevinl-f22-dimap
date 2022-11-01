05bd6e2f611275b933c79536ec82a17c40436033
Kevin Lai <clai@ucsb.edu>
2022-10-31 23:40:35 -0700

    Random graph generator

--- main.cpp
+++ main.cpp
@@ -3,20 +3,36 @@
 #include <chrono>
 #include <random>
 
-int main(int argc, char** argv) {
-    double sum;
-    int rolls = 10000000;
+void print_grid(int grid[], int grid_dim) {
+    for (int i = 0; i < (grid_dim * grid_dim); i++) {
+        printf("%d ", grid[i]);
+        if (i % grid_dim == grid_dim - 1) {
+            printf("\n");
+        }
+    }
+}
+
+void shuffle(int grid[], int grid_dim) {
     unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
-    
     std::default_random_engine generator(seed);
     std::uniform_real_distribution<double> distribution(0.0,1.0);
 
+    for (int i = 0; i < (grid_dim * grid_dim); i++) {
+        if (distribution(generator) < 0.5) {
+            grid[i] = 1;
+        } else {
+            grid[i] = 0;
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    int grid[9];
+    
     printf("hello there!\n");
     
-    for (int i = 0; i < rolls; i++) {
-        sum += distribution(generator);
-    }
-    printf("uniform? %f\n", sum / rolls);
+    shuffle(grid, 3);
+    print_grid(grid, 3);
     
     return 0;
 }
