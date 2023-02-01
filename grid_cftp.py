880e674e1685a16312a706bdd4668dd67b0f0651
Kevin Lai <clai@ucsb.edu>
2023-02-01 15:52:13 -0800

    Beta labels for grid_cftp

--- grid_cftp.py
+++ grid_cftp.py
@@ -3,6 +3,7 @@ import random
 import utils
 import grid_ising as grid
 import matplotlib.pyplot as plt
+from math import log, sqrt
 
 random.seed(None)
 
@@ -14,17 +15,37 @@ shape = (dim, dim)
 step_size = float(sys.argv[2])
 max_iters = int(sys.argv[3])
 
-bvals = []
-bsteps = []
-b_crit =  .44
-
+b_crit = log(1+sqrt(2)) / 2
+plt.xlim(right=(b_crit + step_size))
+plt.ylim(top=max_iters)
 bvals, bsteps = utils.simulate(grid.CFTP(grid.heat_bath_flip, shape, max_iters), step_size)
-plt.plot(bvals, bsteps, label="Heat bath")
+plt.plot(bvals, bsteps, color="darkorange", label="Heat bath")
+plt.annotate(
+    "β={:.2f}".format(bvals[-1]),
+    xy=(bvals[-1], bsteps[-1]),
+    textcoords="offset points",
+    xytext=(0,5),
+    ha='right',
+    color="darkorange")
 bvals, bsteps = utils.simulate(grid.CFTP(grid.metropolis_flip, shape, max_iters), step_size)
-plt.plot(bvals, bsteps, label="Metropolis filter")
+plt.plot(bvals, bsteps, color = "steelblue", label="Metropolis filter")
+plt.annotate(
+    "β={:.2f}".format(bvals[-1]),
+    xy=(bvals[-1], bsteps[-1]),
+    textcoords="offset points",
+    xytext=(0,15),
+    ha='right',
+    color="steelblue")
 plt.xlabel("Temperature β")
 plt.ylabel("Mixing time (in MC steps)")
 plt.title("{:d}x{:d} grid Ising CFTP".format(dim,dim))
 plt.axvline(x = b_crit, color = 'r', linestyle = '-', label="Critical β")
+plt.annotate(
+    "β={:.2f}".format(b_crit),
+    xy=(b_crit, max_iters),
+    textcoords="offset points",
+    xytext=(0, -15),
+    ha='right',
+    color="red")
 plt.legend(loc="upper left")
 plt.savefig("grid_cftp{:d}x{:d}.png".format(dim,dim))
