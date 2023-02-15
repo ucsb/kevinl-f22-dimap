8bd3e9d0408c66f54512c6d18c84289fa60e1b14
Kevin Lai <clai@ucsb.edu>
2023-02-14 21:46:01 -0800

    Glauber Ising test grid

--- cpp/swendsen-wang.cpp
+++ cpp/swendsen-wang.cpp
@@ -1,11 +1,19 @@
 #include <cstdio>
 #include <vector>
 #include <iostream>
+#include <random>
+#include <chrono>
+#include <cmath>
 
 using namespace std;
 
+unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+default_random_engine generator(seed);
+uniform_real_distribution<double> uniform(0.0,1.0);
+
 class Grid {
 public:
+    Grid(int dim, int colors) : Grid(dim, dim, colors) {}
     Grid(int w, int h, int colors) {
         this->w = w;
         this->h = h;
@@ -22,6 +30,14 @@ public:
             delete[] graph[i];
         delete[] graph;
         delete[] counts;
+    }
+    void set(int r, int c, int new_val) {
+        int old_val = this->graph[r][c];
+        if (old_val != new_val) {
+            this->counts[old_val]--;
+            this->counts[new_val]++;
+        }
+        this->graph[r][c] = new_val;
     }
     void set_all(int color) {
         for (int i = 0; i < this->h; i++) {
@@ -62,16 +78,71 @@ public:
         }
         cout << "\n";
     }
+    bool operator==(const Grid& other) {
+        if (this->w != other.w || this->h != other.w) return false;
+        if (this->colors != other.colors) return false;
+        for (int i = 0; i < this->h; i++) {
+            for (int j = 0; j < this->w; j++) {
+                if (this->graph[i][j] != other.graph[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool operator!=(const Grid& other) {
+        return !(*this == other);
+    }
 
     int w, h, size, colors;
     int** graph;
     int* counts;
 };
 
+void choose_point(int dim, int& r, int& c) {
+    r = int(uniform(generator) * dim);
+    c = int(uniform(generator) * dim);
+}
+
+void sum_neighbors(const Grid& g, int& r, int& c, int counts[]) {
+    if (r-1 >= 0)
+        counts[g.graph[r-1][c]]++;
+    if (r+1 < g.h)
+        counts[g.graph[r+1][c]]++;
+    if (c-1 >= 0)
+        counts[g.graph[r][c-1]]++;
+    if (c+1 < g.w)
+        counts[g.graph[r][c+1]]++;
+}
+
+void glauber_ising_flip(Grid& g, int& r, int& c, double& beta, double& rand) {
+    int counts[2];
+    sum_neighbors(g, r, c, counts);
+    double beta_n = beta * counts[0];
+    double beta_p = beta * counts[1];
+    double prob_p = exp(beta_p) / (exp(beta_n) + exp(beta_p));
+    g.set(r, c, (int)(rand <= prob_p));
+}
+
+int glauber_ising_coupling(int dim, double beta, int max_steps) {
+    int r, c, steps = 0;
+    int* counts;
+    double rand;
+    Grid zeros(dim, 2);
+    Grid ones(dim, 2);
+    zeros.set_all(0);
+    ones.set_all(1);
+    while (steps < max_steps && ones != zeros) {
+        choose_point(dim, r, c);
+        rand = uniform(generator);
+        glauber_ising_flip(zeros, r, c, beta, rand);
+        glauber_ising_flip(ones, r, c, beta, rand);
+        steps++;
+    }
+    return steps;
+}
+
 int main(int argc, char** argv) {
-    Grid g(5, 5, 5);
-    g.print();
-    g.chessboard();
-    g.print();
+    printf("%d steps\n", glauber_ising_coupling(100, 0, 1000000));
     return 0;
 }
