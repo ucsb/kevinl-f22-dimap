4cb97260fa1d7063901e6fc28d0b384710b0b748
Kevin Lai <clai@ucsb.edu>
2023-02-15 19:29:50 -0800

    Better plotting

--- plot_steps.py
+++ plot_steps.py
@@ -2,25 +2,30 @@ import sys
 import matplotlib.pyplot as plt
 
 if len(sys.argv) < 2:
-    sys.exit("usage: executable csv-file-name")
+    sys.exit("usage: plot_steps.py csv-file csv-file2 ...")
 
-csv = open(sys.argv[1], 'r').read()
-lines = csv.split('\n')
+for i in range(1, len(sys.argv)):
+    with open(sys.argv[i], 'r') as csv:
+        bvals = []
+        bsteps = []
+        line = csv.readline()
+        while len(line) > 0:
+            beta, steps = line.split(',')
+            bvals.append(float(beta))
+            bsteps.append(float(steps))
+            line = csv.readline()
 
-beta_list = []
-avg_it_list = []
+        plt.plot(bvals, bsteps)
+        
+plt.xlabel("Temperature β")
+plt.ylabel("Mixing time (in MC steps)")
+title = input("Enter title for plot:\n")
+plt.title(title)
 
-for i in range(0, len(lines)):
-    # print(lines[i].split(','))
-    if len(lines[i]) > 0:
-        beta, avg_it = lines[i].split(',')
-        beta_list.append(float(beta))
-        avg_it_list.append(int(avg_it))
-
-plt.plot(beta_list, avg_it_list)
-plt.xlabel("Temperature beta")
-plt.ylabel("Average Markov Chain steps")
-plt.title("Ising model for temperature beta")
-# plt.xlim(left=0, right=25)
-plt.savefig("ising_graph.png", facecolor='white')
-plt.show()
+print("Specific β to plot (enter nothing when done)")
+while True:
+    specific_beta = input()
+    if len(specific_beta) == 0:
+        break
+    plt.axvline(x = float(specific_beta), color = 'r', linestyle = '-', label="Critical β")
+plt.savefig("plot.png", facecolor='white')
