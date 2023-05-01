71cd12da2e0925558c4af373038d99345f0cd6d8
Kevin Lai <clai@ucsb.edu>
2023-05-01 00:29:35 -0700

    Metropolis Glauber chains

---
+++ cpp/metropolis_glauber.cpp
@@ -0,0 +1,145 @@
+#include "metropolis_glauber.hpp"
+
+int Metropolis_Glauber_Grid::run(float beta) {
+    int steps = 0;
+
+    Grid grids[2];
+    grids[0] = Grid(dim, colors);
+    grids[1] = Grid(dim, colors);
+    grids[0].set_all(0);
+    grids[1].set_all(1);
+
+    while (grids[0] != grids[1]) {
+        int point = choose_point(grids[0]);
+        unsigned char color = choose_color(grids[0].colors);
+        float rand = uniform(generator);
+        flip(grids[0], beta, point, color, rand);
+        flip(grids[1], beta, point, color, rand);
+        steps += 1;
+    }
+
+    return steps;
+}
+
+void Metropolis_Glauber_Grid::flip(Grid& g, float beta, int index, unsigned char new_color, float rand) {
+    float prob_accept;
+
+    // Count neighbors of each color for the given vertex
+    int sums[2];
+    sums[0] = 0;
+    sums[1] = 0;
+    sum_neighbors(g, index, sums);
+
+    // Index into neighbor sums based on spin
+    int mchrome_before = sums[g.graph[index]];
+    int mchrome_after = sums[new_color];
+
+    // Calculate acceptance probability
+    prob_accept = std::min(1.0, exp((mchrome_after - mchrome_before) * beta));
+
+    // Accept new state with calculated probability and update grid
+    if (rand <= prob_accept)
+        g.set(index, new_color);
+}
+
+int Metropolis_CFTP_Grid::run(float beta) {
+    int end;
+    std::vector<int> indices;
+    std::vector<float> rands;
+    std::vector<unsigned char> colors;
+    Grid grids[2];
+    for (int c = 0; c < 2; c++) {
+        grids[c] = Grid(dim, 2);
+        grids[c].set_all(c);
+    }
+    indices.push_back(choose_point(grids[0]));
+    rands.push_back(uniform(generator));
+    colors.push_back(2*uniform(generator));
+    while (grids[0] != grids[1]) {
+        end = indices.size() - 1;
+        for (int i = end; i >= 0; i--) {
+            for (int c = 0; c < 2; c++)
+                flip(grids[c], beta, indices[i], colors[i], rands[i]);
+            indices.push_back(choose_point(grids[0]));
+            rands.push_back(uniform(generator));
+            colors.push_back(2*uniform(generator));
+        }
+    }
+    return (int)indices.size();
+}
+
+int Metropolis_Glauber_Complete::run(float beta) {
+    int steps = 0;
+
+    Grid grids[2];
+    grids[0] = Grid(dim, colors);
+    grids[1] = Grid(dim, colors);
+    grids[0].set_all(0);
+    grids[1].set_all(1);
+
+    float beta_scaled = -1 * log(1 - (beta / grids[0].size));
+
+    while (grids[0] != grids[1]) {
+        int point = choose_point(grids[0]);
+        unsigned char color = choose_color(grids[0].colors);
+        float rand = uniform(generator);
+        flip(grids[0], beta_scaled, point, color, rand);
+        flip(grids[1], beta_scaled, point, color, rand);
+        steps += 1;
+    }
+
+    return steps;
+}
+
+void Metropolis_Glauber_Complete::flip(Grid& g, float beta, int index, unsigned char new_color, float rand) {
+    unsigned char old_color = g.graph[index];
+    float prob_accept;
+
+    // Calculate acceptance probability
+    if (old_color == new_color)
+        prob_accept = 1; // exp(0)
+    else {
+        int mchrome_old = 0;
+        int mchrome_new = 0;
+
+        if (g.counts[new_color] > 0)
+            mchrome_new = g.counts[new_color];
+        if (g.counts[old_color] > 1)
+            mchrome_old = 1 - g.counts[old_color];
+
+        prob_accept = std::min(1.0, exp((mchrome_old + mchrome_new) * beta));
+    }
+
+    // Accept new state with calculated probability and update grid
+    if (rand <= prob_accept)
+        g.set(index, new_color);
+}
+
+int Metropolis_CFTP_Complete::run(float beta) {
+    int end;
+    std::vector<int> indices;
+    std::vector<float> rands;
+    std::vector<unsigned char> colors;
+    Grid grids[2];
+    for (int c = 0; c < 2; c++) {
+        grids[c] = Grid(dim, 2);
+        grids[c].set_all(c);
+    }
+
+    float beta_scaled = -1 * log(1 - (beta / grids[0].size));
+
+    indices.push_back(choose_point(grids[0]));
+    rands.push_back(uniform(generator));
+    colors.push_back(2*uniform(generator));
+    while (grids[0] != grids[1]) {
+        end = indices.size() - 1;
+        for (int i = end; i >= 0; i--) {
+            for (int c = 0; c < 2; c++)
+                flip(grids[c], beta_scaled, indices[i], colors[i], rands[i]);
+            indices.push_back(choose_point(grids[0]));
+            rands.push_back(uniform(generator));
+            colors.push_back(2*uniform(generator));
+        }
+    }
+    return (int)indices.size();
+}
---
+++ cpp/metropolis_glauber.hpp
@@ -0,0 +1,35 @@
+#include "chain.hpp"
+#include "grid.hpp"
+
+class Metropolis_Glauber_Grid : public Chain {
+public:
+    Metropolis_Glauber_Grid() : Metropolis_Glauber_Grid(1, 1) {}
+    Metropolis_Glauber_Grid(int dim, int colors) : Chain(dim, 2) {}
+    ~Metropolis_Glauber_Grid() {}
+    virtual int run(float beta) override;
+    void flip(Grid& g, float beta, int index, unsigned char new_color, float rand);
+};
+
+class Metropolis_CFTP_Grid: public Metropolis_Glauber_Grid {
+public:
+    Metropolis_CFTP_Grid() : Metropolis_CFTP_Grid(1) {}
+    Metropolis_CFTP_Grid(int dim) : Metropolis_Glauber_Grid(dim, 2) {}
+    int run(float beta) override;
+};
+
+class Metropolis_Glauber_Complete : public Chain {
+public:
+    Metropolis_Glauber_Complete() : Metropolis_Glauber_Complete(1) {}
+    Metropolis_Glauber_Complete(int dim) : Chain(dim, 2) {}
+    ~Metropolis_Glauber_Complete() {}
+    virtual int run(float beta) override;
+    void flip(Grid& g, float beta, int index, unsigned char new_color, float rand);
+};
+
+class Metropolis_CFTP_Complete : public Metropolis_Glauber_Complete {
+public:
+    Metropolis_CFTP_Complete() : Metropolis_CFTP_Complete(1) {}
+    Metropolis_CFTP_Complete(int dim) : Metropolis_Glauber_Complete(dim) {}
+    ~Metropolis_CFTP_Complete() {}
+    virtual int run(float beta) override;
+};
