ddbf9a802a0296fe0939e8c128fa6c0f2d6db75c
Kevin Lai <clai@ucsb.edu>
2023-05-01 00:33:17 -0700

    Reorganize util functions

--- cpp/utils.cpp
+++ cpp/utils.cpp
@@ -2,26 +2,15 @@
 
 unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 std::default_random_engine generator(seed);
-std::uniform_real_distribution<double> uniform(0.0,1.0);
+std::uniform_real_distribution<float> uniform(0.0,1.0);
 
 // Avoids negative remainders
 int mod(int a, int b) {
     return (b + (a % b)) % b;
 }
 
-int choose_point(const Grid& c) {
-    return int(uniform(generator) * c.size);
-}
-
-int choose_color(int colors) {
-    return (int)(uniform(generator) * colors);
-}
-
-void sum_neighbors(const Grid& g, int& index, int counts[]) {
-    counts[g.graph[mod(index+g.w, g.size)]]++;
-    counts[g.graph[mod(index-g.w, g.size)]]++;
-    counts[g.graph[mod(index-1, g.size)]]++;
-    counts[g.graph[mod(index+1, g.size)]]++;
+unsigned char choose_color(unsigned char colors) {
+    return (unsigned char)(uniform(generator) * colors);
 }
 
 bool keep_edge(double& beta) {
--- cpp/utils.hpp
+++ cpp/utils.hpp
@@ -1,18 +1,13 @@
 #include <random>
 #include <chrono>
-#include "grid.hpp"
 
 extern unsigned seed;
 extern std::default_random_engine generator;
-extern std::uniform_real_distribution<double> uniform;
+extern std::uniform_real_distribution<float> uniform;
 
 // Avoids negative remainders
 int mod(int a, int b);
 
-int choose_point(const Grid& c);
-
-int choose_color(int colors);
-
-void sum_neighbors(const Grid& g, int& index, int counts[]);
+unsigned char choose_color(unsigned char colors);
 
 bool keep_edge(double& beta);
