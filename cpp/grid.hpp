5404ca9565673db89b9433929477f1153feed861
Kevin Lai <clai@ucsb.edu>
2023-04-30 19:48:07 -0700

    Replace size_t with unsigned int

--- cpp/grid.hpp
+++ cpp/grid.hpp
@@ -9,10 +9,10 @@
 class Grid {
 public:
     Grid();
-    Grid(size_t dim, unsigned char colors);
-    Grid(size_t w, size_t h, unsigned char colors);
+    Grid(unsigned dim, unsigned char colors);
+    Grid(unsigned w, unsigned h, unsigned char colors);
     ~Grid();
-    void set(size_t index, unsigned char new_color);
+    void set(unsigned index, unsigned char new_color);
     void set_all(unsigned char color);
     void chessboard();
     void rand();
@@ -24,19 +24,23 @@ public:
     bool operator==(const Grid& other);
     bool operator!=(const Grid& other);
 
-    size_t w, h, size;
+    unsigned w, h, size;
     unsigned char colors;
     unsigned char* graph;
-    size_t* counts;
+    unsigned* counts;
 };
 
-size_t choose_point(const Grid& c);
+unsigned choose_point(const Grid& c);
 
-void sum_neighbors(const Grid& g, size_t index, size_t counts[]);
+void sum_neighbors(const Grid& g, int index, unsigned counts[]);
 
-void print_array(const Grid grids[], size_t size);
+void print_array(const Grid grids[], unsigned size);
 
-void print_array(std::ostream& os, const Grid grids[], size_t size);
+void print_array(const Grid** grids, unsigned size);
+
+void print_array(std::ostream& os, const Grid grids[], unsigned size);
+
+void print_array(std::ostream& os, const Grid** grids, unsigned size);
 
 bool mag_diff(const Grid& g1, const Grid& g2);
 
