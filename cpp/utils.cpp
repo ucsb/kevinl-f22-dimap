bbb3efd9aec7e241baff970ce06c84ccf08ad39f
Kevin Lai <clai@ucsb.edu>
2023-05-09 01:43:23 -0700

    Remove global rng

--- cpp/utils.cpp
+++ cpp/utils.cpp
@@ -1,8 +1,6 @@
 #include "utils.hpp"
 
 unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-std::default_random_engine generator(seed);
-std::uniform_real_distribution<float> uniform(0.0,1.0);
 
 // Avoids negative remainders
 int mod(int a, int b) {
