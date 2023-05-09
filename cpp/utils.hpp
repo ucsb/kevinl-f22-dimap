11ee8efcad948752c8ae68343ef98e09408015e9
Kevin Lai <clai@ucsb.edu>
2023-05-09 01:38:10 -0700

    Utils header guard

--- cpp/utils.hpp
+++ cpp/utils.hpp
@@ -1,3 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
 #include <random>
 #include <chrono>
 
@@ -11,3 +14,5 @@ int mod(int a, int b);
 unsigned char choose_color(unsigned char colors);
 
 bool keep_edge(double& beta);
+
+#endif
