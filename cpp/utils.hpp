c7ce49268da13bd496dd67e9c28b075955dce0a3
Kevin Lai <clai@ucsb.edu>
2023-05-09 01:42:25 -0700

    Convenient debug utils

--- cpp/utils.hpp
+++ cpp/utils.hpp
@@ -3,12 +3,24 @@
 
 #include <random>
 #include <chrono>
+#include <iostream>
 
 extern unsigned seed;
 
 // Avoids negative remainders
 int mod(int a, int b);
 
+template <typename T>
+void print_array(std::ostream& os, T* array, int size) {
+    for (int i = 0; i < size; i++) {
+        os << array[i] << ' ';
+    }
+    os << '\n';
+}
 
+template <typename T>
+void print_array(T* array, int size) {
+    print_array<T>(std::cout, array, size);
+}
 
 #endif
