b677a8b8f45cda0ab5d3f8e1bc478b0ea5da2162
Kevin Lai <clai@ucsb.edu>
2023-05-09 01:39:49 -0700

    Remove global rng

--- cpp/utils.hpp
+++ cpp/utils.hpp
@@ -5,8 +5,6 @@
 #include <chrono>
 
 extern unsigned seed;
-extern std::default_random_engine generator;
-extern std::uniform_real_distribution<float> uniform;
 
 // Avoids negative remainders
 int mod(int a, int b);
