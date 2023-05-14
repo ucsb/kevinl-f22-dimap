7bc6c995eda0b07778a153714e952e68c4f9f720
Kevin Lai <clai@ucsb.edu>
2023-05-14 13:10:13 -0700

    2D array util

--- cpp/utils.hpp
+++ cpp/utils.hpp
@@ -13,16 +13,38 @@ typedef unsigned char color_t;
 int mod(int a, int b);
 
 template <typename T>
-void print_array(std::ostream& os, T* array, int size) {
-    for (int i = 0; i < size; i++) {
+void print_array(std::ostream& os, T* array, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
         os << array[i] << ' ';
     }
     os << '\n';
 }
 
 template <typename T>
-void print_array(T* array, int size) {
+void print_array(std::ostream& os, T* array, int h, int w)
+{
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            os << array[j + (w * i)] << ' ';
+        }
+        os << '\n';
+    }
+}
+
+template <typename T>
+void print_array(T* array, int size)
+{
     print_array<T>(std::cout, array, size);
+}
+
+template <typename T>
+void print_array(T* array, int h, int w)
+{
+    print_array<T>(std::cout, array, h, w);
 }
 
 #endif
